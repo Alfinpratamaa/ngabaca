a1ffcad28e23f9c6171368c650647073a75c0e6b
hnxzl <weafoaley@gmail.com>
2025-06-21 15:15:11 +0700

    configure role based for admin and pelanggan

--- resources/views/components/main-navbar.blade.php
+++ resources/views/components/main-navbar.blade.php
@@ -46,7 +46,7 @@
                     <!-- Profile -->
                     <flux:dropdown position="bottom" align="end" class="text-zinc-950">
                         <flux:profile
-                            avatar="{{ auth()->user()->avatar ?? 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80' }}"
+                            avatar="{{ auth()->user()->avatar ?? '/assets/images/default-avatar.png' }}"
                             name="{{ auth()->user()->name }}" />
                         <flux:navmenu>
                             <flux:navmenu.item icon="user" href="/profile">Profile</flux:navmenu.item>
--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,24 @@
 <?php
 
 use App\Livewire\Home;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
+
 
 Route::get('/', function(){
     return view('home');
 })->name('home');
 
 
-
 Route::middleware(['auth','role:pelanggan'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
-
-    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
-    Volt::route('settings/password', 'settings.password')->name('settings.password');
-    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
-    // Route::view('admin', 'admin.dashboard')->name('admin.dashboard');
+    Route::view('admin/dashboard', 'admin.dashboard')->name('admin.dashboard');
    Route::redirect('settings', 'settings/profile');
 
     Volt::route('settings/profile', 'settings.profile')->name('settings.profile');

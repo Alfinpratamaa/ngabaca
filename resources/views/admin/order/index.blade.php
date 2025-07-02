8c2d5039457a6c57839a9c047cee0af0471cf475
GilangWP <gilangwidyaputra@gmail.com>
2025-07-02 13:49:40 +0700

    Order Admin UI

--- resources/views/admin/order/index.blade.php
+++ resources/views/admin/order/index.blade.php
@@ -1,27 +1,33 @@
 <x-layouts.admin :title="__('Book Admin')">
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <div class="flex items-center justify-between">
-            <h1 class="text-2xl font-bold">{{ __('Book Admin') }}</h1>
+            <h1 class="text-2xl font-bold">{{ __('Order List') }}</h1>
             <a href="{{ route('admin.book.create') }}" class=" p-2 rounded-lg font-bold bg-green-500 hover:bg-green-300">{{ __('Create Book') }}</a>
         </div>
         <div class="overflow-x-auto">
-            <table class="table table-zebra w-full">
-                <thead>
+            <table class="min-w-full bg-white border border-gray-200">
+                <thead class="bg-gray-50">
                     <tr>
-                        <th>#</th>
-                        <th>{{ __('Title') }}</th>
-                        <th>{{ __('Author') }}</th>
-                        <th>{{ __('Published Year') }}</th>
-                        <th>{{ __('Actions') }}</th>
+                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">No</th>
+                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ __('Tanggal Pesanan') }}</th>
+                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ __('Customer') }}</th>
+                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ __('Harga Total') }}</th>
+                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ __('Status Pembayaran')}}</th>
+                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ __('Alamat Pengiriman') }}</th>
+                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ __('Updated-At') }}</th>
+                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ __('Actions') }}</th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody class="bg-white divide-y divide-gray-200">
                     @forelse($orders as $order)
-                        <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $order->title }}</td>
-                            <td>{{ $order->author }}</td>
-                            <td>{{ $order->published_year }}</td>
+                        <tr wire:key="order-{{$order->id}}" class="hover:bg-gray-50">
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{{ $orders->firstItem() + $loop->index }}</td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{{ $order->created_at }}</td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{{ $order->user_id }}</td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{{ $order->total_price }}</td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{{ $order->status }}</td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{{ $order->shipping_address}}</td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{{ $order->updated_at }}</td>
                             <td>
                                 <div class="flex gap-2 items-center">
                                         <flux:button
@@ -35,7 +41,7 @@
                         </tr>
                     @empty
                         <tr>
-                            <td colspan="5" class="text-center">{{ __('No books found') }}</td>
+                            <td colspan="5" class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-center">{{ __('No books found') }}</td>
                         </tr>
                     @endforelse
                 </tbody>
